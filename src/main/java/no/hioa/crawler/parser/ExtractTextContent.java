8e703283c98b3e0722e4a9439092ecc2498f3b58
Alex <ab@prognett.no>
2014-10-29 02:00:48 +0100

    Extracting content from webpages

--- src/main/java/no/hioa/crawler/parser/ExtractTextContent.java
+++ src/main/java/no/hioa/crawler/parser/ExtractTextContent.java
@@ -1,12 +1,18 @@
 package no.hioa.crawler.parser;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -18,14 +24,55 @@ public class ExtractTextContent
 {
 	private static final Logger	consoleLogger	= LoggerFactory.getLogger("stdoutLogger");
 
+	private List<String> stopWords = null;
 	public static void main(String[] args) throws Exception
 	{
 		PropertyConfigurator.configure("log4j.properties");
 		ExtractTextContent extractor = new ExtractTextContent();
 
-		String content = extractor.extractTextContent(new File(
-				"C:/Users/Alex/Desktop/blogs/blogs~/target/0sloasthattumblrcom/0sloasthat_1412332351502"));
-		consoleLogger.info(content);
+		// String content = extractor.extractTextContent(new File(
+		// "C:/Users/Alex/Desktop/blogs/blogs~/target/0sloasthattumblrcom/0sloasthat_1412332351502"));
+		// consoleLogger.info(content);
+
+		extractor.extractFolders(new File("C:/Users/Alex/Desktop/blogs/blogs~/target/"));
+		
+		//System.out.println(StandardAnalyzer.STOP_WORDS_SET.toString());
+	}
+	
+	public ExtractTextContent()
+	{
+		stopWords = getStopWords();
+	}
+
+	public void extractFolders(File folderPath)
+	{		
+		for (File folder : folderPath.listFiles())
+		{
+			if (folder.isDirectory())
+			{
+				StringBuffer buffer = extractFolderContent(folder);
+				consoleLogger.info("Folder {} has {} lines (size: {})", folder.getName(), StringUtils.countMatches(buffer.toString(), "\n"),
+						buffer.length());
+				saveResult(new File(folderPath + "/" + folder.getName() + ".txt"), buffer);
+			}
+		}
+	}
+
+	public StringBuffer extractFolderContent(File folder)
+	{
+		StringBuffer buffer = new StringBuffer();
+
+		for (File file : folder.listFiles())
+		{
+			if (file.isFile())
+			{
+				String content = extractTextContent(file);
+				if (content != null && !StringUtils.isEmpty(content))
+					buffer.append(content);
+			}
+		}
+
+		return buffer;
 	}
 
 	public String extractTextContent(File htmlFile)
@@ -46,14 +93,15 @@ public class ExtractTextContent
 				StringTokenizer words = new StringTokenizer(content);
 				if (words.countTokens() < 5)
 				{
-
+					// ignore
 				}
 				else if (words.countTokens() > 5 && words.countTokens() < 15)
 				{
 					boolean found = false;
 					while (words.hasMoreTokens())
 					{
-						if (StandardAnalyzer.STOP_WORDS_SET.contains(words.nextToken()))
+						if (stopWords.contains(words.nextToken()))
+						//if (StandardAnalyzer.STOP_WORDS_SET.contains(words.nextToken()))
 						{
 							found = true;
 							break;
@@ -76,5 +124,42 @@ public class ExtractTextContent
 			consoleLogger.error("Unknown error", ex);
 			return null;
 		}
+	}
+	
+	private List<String> getStopWords()
+	{
+		return getFileContent(new File("src/main/resources/no/hioa/crawler/parser/stop-words-english.txt"));
+	}
+	
+	private void saveResult(File file, StringBuffer buffer)
+	{
+		try (PrintWriter writer = new PrintWriter(file, "ISO-8859-1"))
+		{			
+			writer.write(buffer.toString());
+		}
+		catch (IOException ex)
+		{
+			consoleLogger.error("Could not save links to file " + file, ex);
+		}
+	}
+	
+	private List<String> getFileContent(File file)
+	{
+		List<String> words = new LinkedList<>();
+
+		try (Scanner scanner = new Scanner(new FileInputStream(file), "ISO-8859-1"))
+		{
+			while (scanner.hasNextLine())
+			{
+				String input = scanner.nextLine().toLowerCase();
+				words.add(input);
+			}
+		}
+		catch (Exception ex)
+		{
+			consoleLogger.error("Could not read content for file " + file.getAbsolutePath(), ex);
+		}
+
+		return words;
 	}
 }
